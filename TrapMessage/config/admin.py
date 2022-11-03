b73c11b70d188a6f20f453be14ab401b5ce1507b
DmytroGorbenko <dimo4ka12345000@gmail.com>
2022-11-03 21:36:55 +0300

    Check

--- TrapMessage/config/admin.py
+++ TrapMessage/config/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-
-
 from .models import Config
 
 
-admin.site.register(Config)
+class ConfigA(admin.ModelAdmin):
+    exclude = ('users',)
+    list_display = ('name', 'ip', 'udp', 'community')
+
+
+admin.site.register(Config, ConfigA)
+admin.site.site_header = "TrapMessage Admin"
